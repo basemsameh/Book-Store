1779740eaa62155c9ddf3b04d70d076e592e4f23
Basem Sameh <basemsameh3000@gmail.com>
2024-11-22 01:44:46 +0200

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component, HostListener } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router,
+  RouterOutlet,
+} from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { CommonModule } from '@angular/common';
@@ -12,7 +19,7 @@ import { CommonModule } from '@angular/common';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'BookStore';
+  title = 'Book Store';
   isVisible = false; // Controls button visibility
 
   // Detect scroll events
@@ -28,5 +35,39 @@ export class AppComponent {
   // Scroll to the top of the page
   scrollToTop(): void {
     window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+  // Part Of Loading Page
+  isLoading = false; // Controls loading page visibility
+  isHiding = false; // Controls fade-out effect
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        this.showLoading();
+      } else if (
+        event instanceof NavigationEnd ||
+        event instanceof NavigationCancel ||
+        event instanceof NavigationError
+      ) {
+        this.hideLoadingWithDelay();
+      }
+    });
+  }
+
+  // Show loading screen
+  private showLoading(): void {
+    this.isLoading = true;
+    this.isHiding = false; // Ensure it's fully visible
+  }
+
+  // Hide loading screen with a fade-out effect
+  private hideLoadingWithDelay(): void {
+    setTimeout(() => {
+      this.isHiding = true; // Start fade-out
+      setTimeout(() => {
+        this.isLoading = false; // Remove from DOM after fade-out
+      }, 500); // Matches the CSS transition duration (0.5s)
+    }, 500); // Ensure loading screen is shown for at least 1 second
   }
 }
